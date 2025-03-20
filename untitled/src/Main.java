8029f8d088377c990312722b79061d154d47590e
lutfensus <tufanyesil@protonmail.com>
2025-03-20 21:58:37 +0100

    TASK 1

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,15 +1,37 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int[] in1 = {7, 8, 9, 10, 11};
+        int[] outArray1 = filterEvenIndexOddValue(in1);
+        System.out.println(java.util.Arrays.toString(outArray1));
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    }
+
+
+   // TASK 1
+    /**
+     * This method filters array elements, keeping only those
+     * that are at even indices (0, 2, 4, ...)
+     * and are simultaneously odd numbers.
+     *
+     * @param array array of integers
+     * @return new array with filtered elements
+     */
+    public static int[] filterEvenIndexOddValue(int[] array) {
+
+        int count = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (i % 2 == 0 && array[i] % 2 == 1) {
+                count++;
+            }
         }
+        int[] out = new int[count];
+        int index = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (i % 2 == 0 && array[i] % 2 == 1 ) {
+                out[index++] = array[i];
+
+            }
+        }
+        return out;
     }
 }
